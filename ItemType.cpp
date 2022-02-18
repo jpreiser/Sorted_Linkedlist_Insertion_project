33837451ffe1db72957fc8d302eaecd4bc17ed81
Jacob Reiser <jpreiser@uga.edu>
2022-02-18 09:44:20 -0500

    First Stab at implementation.

--- ItemType.cpp
+++ ItemType.cpp
@@ -1,1 +1,29 @@
-cpp
+
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include "ItemType.h"
+
+using namepace std;
+
+Comparisson ItemType::compareTo(ItemType item) {
+    Comparisson comp;
+    if (item.getValue() == value) {
+        comp = EQUAL;
+    } else if (item.getValue() > value) {
+        comp = GREATER;
+    } else if (item.getValue() < value) {
+        comp = LESS;
+    }
+    return comp;
+
+} // compareTo
+
+int ItemType::getValue() const {
+    return value;
+} // getValue
+
+void ItemType::initialize(int num) {
+    value = num;
+} //
