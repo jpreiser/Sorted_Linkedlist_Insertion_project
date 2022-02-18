ef978b4374f5a21fd698c2157f89d9b432c385a6
Jacob Reiser <jpreiser@uga.edu>
2022-02-18 13:41:33 -0500

    Switch Cases looping properly.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <limits>
+#include <iterator>
 #include "ItemType.h"
 #include "ListNode.h"
 #include "SortedLinkedList.h"
@@ -10,7 +12,7 @@ using namespace std;
 int main() {
 
 	char choice;
-	while(1) {
+	while(choice != 'q') {
 		cout << "Commands:" << endl;
 		cout << "(i) - Insert value" << endl;
 		cout << "(d) - Delete value" << endl;
@@ -23,34 +25,46 @@ int main() {
 		cout << "(p) - Print list" << endl;
 		cout << "(l) - Print length" << endl;
 		cout << "(q) - Quit program" << endl;
-		cin >> choice;
+		cin.get(choice);
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		switch(choice) {
 		case 'i':
 			cout << "i" << endl;
+			break;
 		case 'd':
 			cout << "d" << endl;
+			break;
 		case 's':
 			cout << "s" << endl;
+			break;
 		case 'n':
 			cout << "n" << endl;
+			break;
 		case 'r':
 			cout << "r" << endl;
+			break;
 		case 'a':
 			cout << "a" << endl;
+			break;
 		case 'm':
 			cout << "m" << endl;
+			break;
 		case 't':
 			cout << "t" << endl;
+			break;
 		case 'p':
 			cout << "p" << endl;
+			break;
 		case 'l':
 			cout << "l" << endl;
-		case 'q':
 			break;
+		case 'q':
+			return 0;
 		default:
 			cout << "Invalid command, try again!" << endl;
+			break;
 		}
 	}
-
 	return 0;
 } //main
